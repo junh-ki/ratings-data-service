03fa5813d3862ebce49bff754b8bf6e0fcdcb1c8
Junhyung Ki <kijoonh91@gmail.com>
2022-04-16 09:15:06 +0200

    Implement a hard-coded API /ratingsdata/users/{userId}

--- src/main/java/io/msa/ratingsdataservice/resources/RatingsResource.java
+++ src/main/java/io/msa/ratingsdataservice/resources/RatingsResource.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsResource {
@@ -12,6 +15,11 @@ public class RatingsResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("users/{userId}")
+    public List<Rating> getUserRatings(@PathVariable("userId") String userId) {
+        return Arrays.asList(new Rating("1234", 4), new Rating("5678", 3));
     }
 
 }
